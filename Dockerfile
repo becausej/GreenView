f6e7e4d84b5977bf8e2ce1225b91dc49c90dea02
Tripp Lyons <tripplyons@gmail.com>
2024-11-09 13:16:41 -0500

    dockerize and move output files

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.11
+WORKDIR /app
+RUN pip install rebrowser-playwright
+RUN rebrowser_playwright install chromium
+RUN rebrowser_playwright install-deps
+ADD requirements.txt /app/
+WORKDIR /usr/bin
+RUN apt-get update -y \
+  && apt-get install --no-install-recommends -y xvfb libgl1-mesa-dri xauth \
+  && rm -rf /var/lib/apt/lists/*
+COPY xvfb-startup.sh .
+RUN sed -i 's/\r$//' xvfb-startup.sh
+ARG RESOLUTION="1920x1080x24"
+ENV XVFB_RES="${RESOLUTION}"
+ARG XARGS=""
+ENV XVFB_ARGS="${XARGS}"
+ENTRYPOINT ["/bin/bash", "xvfb-startup.sh"]
+WORKDIR /app
+ADD main.py /app
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from playwright.sync_api import sync_playwright, Browser, BrowserContext
+from rebrowser_playwright.sync_api import sync_playwright, Browser, BrowserContext
 
 class USGSMapScraper:
     URL = "https://earthexplorer.usgs.gov/"
     RADIUS = "100"
 
-    def __init__(self, headless: bool = False):
+    def __init__(self):
         self.playwright = sync_playwright().start()
-        self.browser: Browser = self.playwright.chromium.launch(headless=headless)
+        self.browser: Browser = self.playwright.chromium.launch(
+            headless=False,
+        )
         self.context: BrowserContext = self.browser.new_context()
+        self.page = self.context.new_page()
+        self.page.goto(self.URL)
+        self.page.wait_for_selector("#tabCircle")
+        self.page.click("#tabCircle")
+        self.page.wait_for_selector("#centerLat")
 
-    def capture(self, latitude: str, longitude: str, output_path: str = "screenshot.png"):
+    def capture(self, latitude: str, longitude: str, output_path: str = "output/screenshot.png"):
         """
         Capture a screenshot of the USGS Earth Explorer map for given coordinates
 
@@ -18,28 +25,21 @@ class USGSMapScraper:
             longitude (str): The longitude coordinate
             output_path (str): Path where the screenshot will be saved
         """
-        page = self.context.new_page()
-        page.goto(self.URL)
-        page.wait_for_selector("#tabCircle")
-        page.click("#tabCircle")
-        page.wait_for_selector("#centerLat")
-        page.locator("#centerLat").press_sequentially(latitude)
-        page.locator("#centerLng").press_sequentially(longitude)
-        page.fill("#circleRadius", self.RADIUS)
-        page.click("#circleEntryApply")
+        self.page.locator("#centerLat").press_sequentially(latitude)
+        self.page.locator("#centerLng").press_sequentially(longitude)
+        self.page.fill("#circleRadius", self.RADIUS)
+        self.page.click("#circleEntryApply")
 
         # hover over #map and scroll up to zoom in
-        page.hover("#map")
-        page.mouse.wheel(0, -1000)
-        page.click("#circleEntryClear")
-        page.wait_for_timeout(500)
-        page.locator("#map").screenshot(path=output_path)
-        page.close()
+        self.page.click("#circleEntryClear")
+        self.page.wait_for_timeout(1000)
+        self.page.locator("#map").screenshot(path=output_path)
 
     def close(self):
         """
         Close the browser and playwright instance
         """
+        self.page.close()
         self.browser.close()
         self.playwright.stop()
 
@@ -50,7 +50,7 @@ class USGSMapScraper:
         self.close()
 
 def main():
-    with USGSMapScraper(headless=False) as scraper:
+    with USGSMapScraper() as scraper:
         for i, (lat, long) in enumerate([
             ('42.3498476225924', '-71.06367409229279'),
             ('42.355367352364375', '-71.0640549659729'),
@@ -58,8 +58,11 @@ def main():
             ('42.3617203219366', '-71.06894731521606'),
             ('42.364225392938955', '-71.06598615646362'),
         ]):
-            scraper.capture(lat, long, f"screenshot_{i}.png")
+            print(f"Capturing screenshot {i}")
+            scraper.capture(lat, long, f"output/screenshot_{i}.png")
 
+    import os
+    print(os.listdir("/app/output"))
 
 if __name__ == "__main__":
     main()
